7ce9eb6c7b8725af4fd91808ce725788a7dbda5d
Xin Li <xin@centml.ai>
2023-02-08 09:52:38 -0500

    do not bring cuda 16 in ld library path

--- setup.sh
+++ setup.sh
@@ -1,17 +1,17 @@
 source /etc/profile.d/lmod.sh
 
-module load cuda-11.6
+module purge
 module load anaconda/3.9
 module load vscode-server/20220909
 
 nvidia-smi
 
-echo CUDA  : `which nvcc`: `nvcc --version`
-echo Python: `which python`: `python --version`
-
 # Install skyline
 python3 -m venv $HOME/centml_tools
+module unload  anaconda/3.9
 source $HOME/centml_tools/bin/activate
+echo CUDA  : `which nvcc`: `nvcc --version`
+echo Python: `which python`: `python --version`
 
 cd $HOME/centml_tools/
 
@@ -24,11 +24,11 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/pkgs/cuda-11.6/extras/CUPTI/lib64/
 pip install http://centml-releases.s3-website.us-east-2.amazonaws.com/habitat/wheels-cu116/habitat_predict-1.0.0-0+cu116-py39-none-any.whl
 
 # Visual Studio Extention
-# Start code server
 cd ./project
 curl http://centml-releases.s3-website.us-east-2.amazonaws.com/skyline-vscode/skyline-vscode-0.0.1.vsix --output skyline-vscode-0.0.1.vsix
 #code-server --install-extension skyline-vscode-0.0.1.vsix
 
+# Start code server
 skyline interactive &
 code-server 
 
