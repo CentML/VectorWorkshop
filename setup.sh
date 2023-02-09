d1db8101ffc60e0e06aa1ac9ee39bc415e0ac38b
Xin Li <xin@centml.ai>
2023-02-09 09:55:52 -0500

    modify scripts

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 source /etc/profile.d/lmod.sh
 
 module purge
@@ -6,17 +8,25 @@ module load vscode-server/20220909
 
 nvidia-smi
 
+LOCAL=/tmp
 # Install skyline
-python3 -m venv $HOME/centml_tools
+python3 -m venv $LOCAL/centml_tools
+cd $LOCAL/centml_tools
 module unload  anaconda/3.9
-source $HOME/centml_tools/bin/activate
+source $LOCAL/centml_tools/bin/activate
 echo Python: `which python`: `python --version`
 
-cd $HOME/centml_tools/
+pip install torch torchvision  --no-cache-dir --extra-index-url https://download.pytorch.org/whl/cu116
+pip install ipywidgets jupyterlab notebook
+
+git clone https://github.com/NVIDIA/apex
+cd apex
+export CUDA_HOME=/pkgs/cuda-11.6/
+pip install -v --disable-pip-version-check --no-cache-dir --global-option="--cpp_ext" --global-option="--cuda_ext" ./
 
 # Skyline
 git clone https://github.com/centml/skyline.git
-pip install -e skyline
+pip install -e skyline --extra-index-url https://download.pytorch.org/whl/cu116
 
 # Habitat
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/pkgs/cuda-11.6/extras/CUPTI/lib64/
@@ -25,10 +35,12 @@ pip install http://centml-releases.s3-website.us-east-2.amazonaws.com/habitat/wh
 # Visual Studio Extention
 cd ./project
 curl http://centml-releases.s3-website.us-east-2.amazonaws.com/skyline-vscode/skyline-vscode-0.0.1.vsix --output skyline-vscode-0.0.1.vsix
-#code-server --install-extension skyline-vscode-0.0.1.vsix
 
 # Start code server
-skyline interactive &
+#port=$(python -c "import socket;s = socket.socket(socket.AF_INET, socket.SOCK_STREAM);s.bind(('', 0));addr = s.getsockname();print(addr[1]);s.close()")
+
+skyline interactive  & #--port $port &
 code-server 
+
 
 
